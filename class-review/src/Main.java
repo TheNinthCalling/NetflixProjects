9ff02bddc8f2f61e918ac0c0fc2d4c87ab775001
TheNinthCalling <98507662+TheNinthCalling@users.noreply.github.com>
2025-07-10 12:59:52 -0500

    Java class review

--- class-review/src/Main.java
+++ class-review/src/Main.java
@@ -1,16 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        //Create an instance of the person class
-        Person jim = new Person();
-        //Set the name, as the variable has public scope
-        jim.name = "Jim";
+        //Refactor the code to use the new constructor and getters/setters
 
-        //Create another instance of the person class called callie and repeat
-        Person callie = new Person();
-        callie.name = "Callie";
+        //Create an instance of the person class named jim
+        Person jim = new Person("Jim");
 
-        //Call their public functions
+        //Create and instance of the person class named callie
+        Person callie = new Person("Callie");
+
+        //Call their respective hello methods
         jim.sayHello();
         callie.sayHello();
+
+        //Print out the created dates
+        System.out.println("Jim was created at: " + jim.getCreatedDate());
+        System.out.println("Callie was created at: " + callie.getCreatedDate());
     }
 }
--- class-review/src/Person.java
+++ class-review/src/Person.java
@@ -1,17 +1,41 @@
+import java.util.Date;
+
 public class Person {
-    //
+    //Change scope to private, allowing modification only within class or public setters
     private String name;
     //Public scope variable allowing modification outside of this file
+
+    //Include a new Date type variable, requires importing the date type from java common utilities
+    private Date createdDate;
 
     //Define a constructor, no modifiers, same name as the class
     //Defining a constructor overrides the base empty constructor
     Person(String name){
         this.name = name;
+        this.createdDate = new Date();
+        //Returns a date based on the time the function was called
     }
 
     //Public function allowing usage outside of this file's scope
     //Non-static method, i.e instance dependent, allowing "this" keyword
     public void sayHello(){
         System.out.println("Hello, I am " + this.name);
+    }
+
+    //Define getters and setters
+
+    public void setName(String name){
+        //This key word refers to the specific instantiation of the class calling this method
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+        //Much like the setter, refers to the specific instance of the class, takes no input, returns a string
+    }
+
+    //Create a getter for the new variable
+    public Date getCreatedDate(){
+        return this.createdDate;
     }
 }
